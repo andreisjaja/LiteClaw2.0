fa56bda4fd789995c9cb9180da13fcad322ddef4
Airin-Paynemil <airin.paynemil.epet20@gmail.com>
2024-10-29 18:27:57 -0300

    se agrego el firebase

---
+++ app/utils/FirebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyshmPwrJxb10gf2BbyeJvCOHdj7WjWPM",
+  authDomain: "liteclaw-69bb1.firebaseapp.com",
+  projectId: "liteclaw-69bb1",
+  storageBucket: "liteclaw-69bb1.appspot.com",
+  messagingSenderId: "706472308881",
+  appId: "1:706472308881:web:0718d6151ca3143c844f30",
+  measurementId: "G-K708SRSDCB"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const db = getFirestore(FIREBASE_APP);
